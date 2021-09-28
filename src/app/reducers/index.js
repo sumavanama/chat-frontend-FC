63d3252a071ef0cf7f8d7ea28d83e7b686b9b62c
maddithanuja20 <maddithanuja20@gmail.com>
2021-09-28 18:32:27 +0530

    routing

--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -11,7 +11,6 @@ const initialState = {
 const userReducer = (state = initialState, action) => {
     switch (action.type) {
         case "SUBMIT_REGISTER":
-            console.log("user register");
             return Object.assign({}, state, { userDetails: action.details });
 
         default: return state;
